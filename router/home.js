be06dac8fd91ed40ce87dd8d3086a3f224e4e22f
vignesh-vic <108075655+vignesh-vic@users.noreply.github.com>
2023-10-18 16:54:59 +0530

    Add files via upload

---
+++ router/home.js
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+export default function home() {
+  return (
+    <>
+    <div className="text-left p-0">
+    hai
+      <Typography variant='h3'>home</Typography>
+      <Typography >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae error distinctio ut quos in alias doloribus, maiores deserunt natus delectus laboriosam non consectetur fugiat excepturi nam esse quibusdam dolor dolorum.
+ </Typography>
+ </div>
+    </>
+  )
+}
